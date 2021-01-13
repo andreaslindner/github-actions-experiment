39a10a49c97a6c14f80352efcd3c502985bb182b
Andreas Lindner <andili@kth.se>
2021-01-13 01:16:47 +0100

    try 24

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "${HOLBA_OPT_DIR}"
 mkdir -p "${HOLBA_OPT_DIR}"
 ls -la "${HOLBA_OPT_DIR}"
 echo "installing now"
-mkdir "${HOLBA_OPT_DIR}/$(date +%Y_%m_%d__%H_%M_%S)"
+mkdir "${HOLBA_OPT_DIR}/${HOLBA_POLYML_VERSION}__$(date +%Y_%m_%d__%H_%M_%S)"
 sleep 5
 date
 
